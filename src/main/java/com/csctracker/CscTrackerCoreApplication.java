d199571e7149d06ca9a6082453f1d342826b8142
Carlos Eduardo Duarte Schwalm <krlsedu@gmail.com>
2022-04-14 09:55:18 -0300

    Initial commit

---
+++ src/main/java/com/csctracker/CscTrackerCoreApplication.java
@@ -0,0 +1,13 @@
+package com.csctracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CscTrackerCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CscTrackerCoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/csctracker/CscTrackerCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csctracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CscTrackerCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
