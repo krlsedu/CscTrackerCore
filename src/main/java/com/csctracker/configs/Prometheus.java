e0e1065f6fd7b61f4b44df4b89f85c31bf9ae447
Carlos Eduardo Duarte Schwalm <krlsedu@gmail.com>
2022-06-15 10:29:24 -0300

    metrics

--- src/main/java/com/csctracker/configs/Prometheus.java
+++ src/main/java/com/csctracker/configs/Prometheus.java
@@ -1,30 +1,19 @@
 package com.csctracker.configs;
 
 import io.micrometer.core.instrument.MeterRegistry;
-import org.apache.maven.model.Model;
-import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
-import java.io.FileReader;
-
 @Component
 public class Prometheus {
+
+    @Value("${spring.application.name}")
+    private String appName;
+
     @Bean
     MeterRegistryCustomizer<MeterRegistry> metricsCommonTags() {
-        var reader = new MavenXpp3Reader();
-        var name = "CscTrackerCore";
-        Model model = null;
-        try {
-            model = reader.read(new FileReader("pom.xml"));
-            name = model.getName();
-            System.out.println(model.getArtifactId());
-            System.out.println(model.getVersion());
-        } catch (Exception e) {
-            //ignore
-        }
-        var finalName = name;
-        return registry -> registry.config().commonTags("application", finalName);
+        return registry -> registry.config().commonTags("application", appName);
     }
 }
