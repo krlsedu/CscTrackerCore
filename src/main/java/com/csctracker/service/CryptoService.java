0391d48f65516549d4017cb7f01bed97b39d7024
Carlos Eduardo Duarte Schwalm <krlsedu@gmail.com>
2023-08-01 14:04:25 -0300

    CryptoUtil

---
+++ src/main/java/com/csctracker/service/CryptoService.java
@@ -0,0 +1,42 @@
+package com.csctracker.service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.Cipher;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.SecretKeySpec;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+
+@Component
+public class CryptoService {
+    @Value("${crypto.key}")
+    private String cryptoKey;
+
+    public String decrypt(String text) throws Exception {
+        var cipher = getCipher(Cipher.DECRYPT_MODE);
+        var decoder = Base64.getDecoder();
+        byte[] encrypted = decoder.decode(text);
+        byte[] decrypted = cipher.doFinal(encrypted);
+        return new String(decrypted);
+    }
+
+    public String encrypt(String text) throws Exception {
+        var cipher = getCipher(Cipher.ENCRYPT_MODE);
+        byte[] encrypted = cipher.doFinal(text.getBytes());
+        var encoder = Base64.getEncoder();
+        return encoder.encodeToString(encrypted);
+    }
+
+    private Cipher getCipher(int mode) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException {
+        if (cryptoKey == null) {
+            throw new RuntimeException("Exception: cryptoKey is null");
+        }
+        var aesKey = new SecretKeySpec(cryptoKey.getBytes(), "AES");
+        var cipher = Cipher.getInstance("AES");
+        cipher.init(mode, aesKey);
+        return cipher;
+    }
+}
